07237ef27d9095187f5edc1a589be127e2ef90f3
lookstar <2023590960@qq.com>
2018-05-10 21:34:23 +0800

    finish the prove of concepts

--- pkg/provider/testProvider.go
+++ pkg/provider/testProvider.go
@@ -8,7 +8,10 @@ import (
 	"net/http"
 	"gopkg.in/headzoo/surf.v1"
 	"github.com/headzoo/surf/browser"
-	//"github.com/PuerkitoBio/goquery"
+	"github.com/robertkrimen/otto"
+	"os"
+	"io"
+	"runtime"
 )
 
 type CollectorProvider struct {
@@ -17,8 +20,8 @@ type CollectorProvider struct {
 }
 
 type Credential struct {
-	Username string
-	Password string
+	Username string	`json:"username"`
+	Password string	`json:"password"`
 }
 
 func NewCollectorProvider(url, output string) *CollectorProvider {
@@ -40,6 +43,11 @@ func (p *CollectorProvider) ReadCredential() *Credential {
 }
 
 func (p *CollectorProvider) handleMediaInitForm(bow *browser.Browser) error {
+	err := bow.Open("https://accounts.sap.com/saml2/idp/usso/sap?sp=video.sap.com")
+	if err != nil {
+		panic(err)
+	}
+
 	form, err := bow.Form("#logOnForm")
 	if err != nil {
 		return err
@@ -52,39 +60,38 @@ func (p *CollectorProvider) handleMediaInitForm(bow *browser.Browser) error {
 	return err
 }
 
-func (p *CollectorProvider) DoWork() error {
-	fmt.Println("hello world!\n")
-
-	bow := surf.NewBrowser()
-
-	clientCrt, _ := tls.LoadX509KeyPair("./data/test.crt", "./data/test.key")
-	tr := &http.Transport{
+func (p *CollectorProvider) getTr() *http.Transport {
+	return &http.Transport{
 		TLSClientConfig: &tls.Config{
 			InsecureSkipVerify: true,
-			Certificates:       []tls.Certificate{clientCrt},
 			Renegotiation:      tls.RenegotiateFreelyAsClient,
 			MaxVersion:         tls.VersionTLS10,
 		},
 	}
+}
 
-	bow.SetTransport(tr)
+func (p *CollectorProvider) constructBrowser() *browser.Browser {
+	bow := surf.NewBrowser()
+	bow.SetTransport(p.getTr())
+	return bow
+}
 
-	err := bow.Open("https://accounts.sap.com/saml2/idp/usso/sap?sp=video.sap.com")
+func (p *CollectorProvider) DoWork() error {
+	fmt.Println("hello world!\n")
+
+	bow := p.constructBrowser()
+	err := p.handleMediaInitForm(bow)
 	if err != nil {
 		panic(err)
 	}
 
-	err = p.handleMediaInitForm(bow)
-	if err != nil {
-		panic(err)
-	}
-
-	if bow.Title() == "Error" {
-		bow.Back()
+	for bow.Title() == "Error" {
+		bow = p.constructBrowser()
 		err = p.handleMediaInitForm(bow)
 		if err != nil {
 			panic(err)
 		}
+		fmt.Println(bow.Title())
 	}
 
 	form, err := bow.Form("#samlRedirect")
@@ -99,13 +106,91 @@ func (p *CollectorProvider) DoWork() error {
 
 	fmt.Println(bow.Title())
 	
-	err = bow.Open("https://video.sap.com/media/t/1_q7ykdtqu#")
+	//part 2
+	//err = bow.Open("https://video.sap.com/media/t/1_4i0i6naj")
+	err = bow.Open("https://video.sap.com/media/t/1_1yr8abip")
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(bow.Title())
+	selection := bow.Find("#player")
+	lastScript := selection.Find("script").Last()
+	vm := otto.New()
+
+	_, err = vm.Run("var kms_kWidgetJsLoader = {};")
+	_, err = vm.Run("kms_kWidgetJsLoader.embed = function(id, kms) {};")
+
+	_, err = vm.Run(lastScript.Text())
+	if err != nil {
+		panic(err)
+	}
+
+	pdata, err := vm.Get("kMainPlayerEmbedObject")
+	if err != nil {
+		panic(err)
+	}
+
+	pdataInterface, _ := pdata.Export()
+	pdataMap := pdataInterface.(map[string]interface{})
+
+	entryId := pdataMap["entry_id"]
+	//fmt.Println(pdataMap["wid"])
+	ks := pdataMap["flashvars"].(map[string]interface{})["ks"].(string)
+
+	//part 3
+	urlPattern := "http://cdnapi.kaltura.com/p/1921661/sp/0/playManifest/entryId/%s/format/url/protocol/http/flavorParamId/301971/ks/%s/video.mp4"
+	targeturl := fmt.Sprintf(urlPattern, entryId, ks)
+
+	fmt.Println(targeturl)
+
+	DownloadBody(&http.Client{Transport: p.getTr()}, targeturl, "c:\\download", "b.mp4")
 
 	return nil
 }
 
+func DownloadBody(client *http.Client, url, dir, filename string) error {
+	os.MkdirAll(dir, os.ModePerm)
+
+	var path string
+	if runtime.GOOS == "windows" {
+		path = dir + "\\" + filename
+	} else {
+		path = dir + "/" + filename
+	}
+
+	file, err := os.Open(path)
+	if err == nil {
+		stat, _ := file.Stat()
+		if stat.Size() > 10 {
+			defer file.Close()
+			return nil
+		}
+	} else {
+		//file not exist
+		if os.IsNotExist(err) {
+			file.Close()
+		} else {
+			return err
+		}
+	}
+
+	out, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+
+	defer out.Close()
+
+	resp, err := client.Get(url)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+	return nil
+}
